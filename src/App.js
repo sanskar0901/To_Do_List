cb2801bf59e7c1b259261e1d4fcdd47bb941db6b
Sanskar Gupta <72657115+sanskar0901@users.noreply.github.com>
2021-03-27 11:25:16 +0530

    error fixed

--- src/App.js
+++ src/App.js
@@ -9,22 +9,23 @@ function App() {
   const [filteredTodos, setFilteredTodos] = useState([]);
   
     useEffect(()=>{
+      const filterHandler =() =>{
+        switch(status){
+          case 'completed':
+            setFilteredTodos(todos.filter((todo) => todo.completed === true))
+            break;
+          case 'uncompleted':
+            setFilteredTodos(todos.filter((todo) => todo.completed === false))
+            break;
+          default:
+            setFilteredTodos(todos);
+            break;
+          }
+    
+      };
       filterHandler();  
     },[todos,status]);
-  const filterHandler =() =>{
-    switch(status){
-      case 'completed':
-        setFilteredTodos(todos.filter((todo) => todo.completed === true))
-        break;
-      case 'uncompleted':
-        setFilteredTodos(todos.filter((todo) => todo.completed === false))
-        break;
-      default:
-        setFilteredTodos(todos);
-        break;
-      }
-
-  };
+ 
   return (
     <div className="App">
      <header>
