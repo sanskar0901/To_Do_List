99ba8c68e114ed9ef6ed61f95a79e646dd98bba8
Sanskar Gupta <72657115+sanskar0901@users.noreply.github.com>
2021-03-27 11:03:01 +0530

    drop box function

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Form = ({setInputText,todos,setTodos,inputText}) =>{
+const Form = ({setInputText,todos,setTodos,inputText,setStatus}) =>{
     const inputTextHandler =(e)=>{
-        console.log(e.target.value);
+        
         setInputText(e.target.value);
     };
     const submitTodoHandler=(e) =>{
@@ -11,6 +11,10 @@ const Form = ({setInputText,todos,setTodos,inputText}) =>{
             ...todos, {text: inputText, completed:false,id: Math.random()*1000}
         ]);
         setInputText("");
+    };
+    const statusHandler =(e)=>{
+      setStatus(e.target.value);
+           
     }
     return(
         <form>
@@ -19,7 +23,7 @@ const Form = ({setInputText,todos,setTodos,inputText}) =>{
           <i onClick={submitTodoHandler} className="fas fa-plus-square"></i>
         </button>
         <div className="select">
-          <select name="todos" className="filter-todo">
+          <select onChange={statusHandler} name="todos" className="filter-todo">
             <option value="all">All</option>
             <option value="completed">Completed</option>
             <option value="uncompleted">Uncompleted</option>
